fad840a441bed64f41e17574d725034afbdffde4
cihadozdemir <ciha0276@stud.kea.dk>
2023-03-13 21:00:49 +0100

    Initial commit

---
+++ src/main/java/com/example/superheltev_four/SuperheltevFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.superheltev_four;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperheltevFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperheltevFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/superheltev_four/SuperheltevFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superheltev_four;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperheltevFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
